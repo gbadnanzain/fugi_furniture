8190d318ebbb0cb560d55038cc08a8a5ffea8629
A. Zain <57663136+gbadnanzain@users.noreply.github.com>
2022-11-12 22:32:49 +0700

    commit

---
+++ lib/app_tyles.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const Color kBrown = Color(0xffEEA427);
+
+const Color kDarkBrown = Color(0xff80450A);
+
+const Color kLightGrey = Color(0xffE3E3E3);
+const Color kGrey = Color(0xff9A9390);
+const Color kWhite = Color(0xffffffff);
+const Color kBlack = Color(0xff111111);
+
+const double kBorderRadius = 40.0;
+const double kPaddingHorizontal = 24.0;
+final kInput
